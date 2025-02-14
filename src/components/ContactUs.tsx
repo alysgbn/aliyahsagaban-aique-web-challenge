f80d52c2d91fc2172fb3fbbb7a40ada7fcb784c0
alysgbn <aliyah.sagaban22@gmail.com>
2025-02-14 10:36:24 +0800

    Polished

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -10,19 +10,19 @@ const ContactUs = () => {
         {/* Contact us section header */}
         <div className="flex flex-col gap-1 md:gap-3 lg:gap-3">
           <p className="text-base font-bold text-primary-blue">Contact us</p>
-          <p className="lg:text-xxl whitespace-nowrap text-xl font-semibold md:text-xl">Get In Touch With Us</p>
+          <p className="lg:text-xxl whitespace-nowrap text-xl font-semibold md:text-xl lg:tracking-wide">Get In Touch With Us</p>
           <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius tempor</p>
         </div>
 
         {/* Loop location thrice and render */}
-        <div className="flex flex-col gap-[10px]">
+        <div className="flex flex-col gap-[10px] mt-1">
           {Array(3)
             .fill(true)
             .map((_, i) => (
               <div key={i} className="flex gap-3">
-                <ButtonGroup icon={<Image src={HouseSvg} alt="House Icon" loading="lazy"/>} />
+                <ButtonGroup icon={<Image src={HouseSvg} alt="House Icon" loading="lazy" />} />
                 <div>
-                  <p className="text-base font-bold">Our Location</p>
+                  <p className="!text-sm font-bold">Our Location</p>
                   <p>One World Place, Lane R, BGC</p>
                 </div>
               </div>
